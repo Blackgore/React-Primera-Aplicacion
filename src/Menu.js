f77cd7b4012ed46f063420a3f42ed22055bfa54d
Daniel Perez <danieljpu2013@gmail.com>
2020-05-14 21:43:16 -0400

    Primer commit

---
+++ src/Menu.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Search from './Search';
+import './Menu.css';
+import PanelAdd from './PanelAdd';
+import { render } from '@testing-library/react';
+
+
+class Menu extends React.Component{
+    
+    constructor (props){
+        super(props);
+        this.state = {newItemPanel: false};
+
+        this.add = this.add.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+    }
+
+    add(){
+        this.setState({newItemPanel: true});
+    }
+
+    onCancel(){
+        this.setState({newItemPanel: false});
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="subcontainer">
+                    <div className="logo">
+                        {this.props.title}
+                    </div>
+
+                    <div className="search">
+                        <Search onsearch={this.props.onsearch}/>
+                    </div>
+
+                    <div className="actions">
+                        <button onClick={this.add} className="button btn-blue">+ Añadir Libro</button>
+                    </div>
+                </div>
+                {
+                    (this.state.newItemPanel)?
+                        <PanelAdd oncancel={this.onCancel} onadd={this.props.onadd} />
+                    :
+                    ''
+                }
+                
+            </div>
+        );
+    }
+}
+
+export default Menu;
